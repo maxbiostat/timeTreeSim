0e8c698c8aaf630d1a92aa5bd166b6f6f926cbad
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2016-07-28 18:01:12 +0100

    UGLY AF but works

--- R/simuTimeTree.r
+++ R/simuTimeTree.r
@@ -51,7 +51,7 @@ simuTimeTree <- function(
     runCommand <- paste("beast -overwrite -seed", SEED,  XMLName)
   }
   system(runCommand, ignore.stdout = !verbose)
-  res <- ape::read.nexus(paste("/tmp/",sub(".xml", ".tree", strsplit(fastaName, "/")[[1]][[2]]), sep = ""))
+  res <- ape::read.nexus(sub(".fasta", ".tree", strsplit(fastaName, "/")[[1]][[2]]))
   setwd(oriD)
   return(res)
 }
