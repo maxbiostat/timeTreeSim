87f7aeddf2d2bd25551bf3b1569682b83a0a8b32
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2016-07-28 17:47:29 +0100

    hacking away

--- R/simuTimeTree.r
+++ R/simuTimeTree.r
@@ -51,7 +51,7 @@ simuTimeTree <- function(
     runCommand <- paste("beast -overwrite -seed", SEED,  XMLName)
   }
   system(runCommand, ignore.stdout = !verbose)
-  res <- ape::read.nexus(sub(".xml", ".tree", XMLName.short))
+  res <- ape::read.nexus(sub(".xml", ".tree", strsplit(XMLName.short, "/")[[1]][[2]]))
   setwd(oriD)
   return(res)
 }
